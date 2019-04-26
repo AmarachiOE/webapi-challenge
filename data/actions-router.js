0f2fa92a1a84044999a0c7a12264b2c1e7c0857f
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-26 08:52:58 -0700

    Set up files for project. Server is running.

---
+++ data/actions-router.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const actions = require("./helpers/actionModel.js");
+
+const actionsRouter = express.Router();
+
+/*
+
+get,
+insert,
+update,
+remove,
+
+*/
+
+// url begins with /api/actions
+
+// GET ALL ACTIONS =================
+// POST  =================
+// PUT  =================
+// DELETE  =================
+
+module.exports = actionsRouter;
---
+++ data/projects-router.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const projects = require("./helpers/projectModel.js");
+
+const projectsRouter = express.Router();
+
+/*
+
+get,
+insert,
+update,
+remove,
+getProjectActions,
+
+*/
+
+// url begins with /api/projects
+
+// GET ALL PROJECTS =================
+// GET PROJECT ACTIONS =================
+// POST  =================
+// PUT  =================
+// DELETE  =================
+
+module.exports = projectsRouter;
